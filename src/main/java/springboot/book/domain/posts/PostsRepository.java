5230ebb016a6a7d45e2f66f7092d1b43661a4b87
becca4011 <becca4011@naver.com>
2022-01-11 16:26:24 +0900

    4장

--- src/main/java/springboot/book/domain/posts/PostsRepository.java
+++ src/main/java/springboot/book/domain/posts/PostsRepository.java
@@ -1,6 +1,11 @@
 package springboot.book.domain.posts;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 // Entity Repository (Entity 클래스와 같은 위치에 있어야 함)
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/springboot/book/service/posts/PostsService.java
+++ src/main/java/springboot/book/service/posts/PostsService.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import springboot.book.domain.posts.Posts;
 import springboot.book.domain.posts.PostsRepository;
+import springboot.book.web.dto.PostsListResponseDto;
 import springboot.book.web.dto.PostsResponseDto;
 import springboot.book.web.dto.PostsSaveRequestDto;
 import springboot.book.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +33,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지, 조회 기능만 남겨두어 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // postsRepository의 결과로 넘어온 Posts의 Stream을 map을 통해 PostsResponseDto(List)로 변환
+                .collect(Collectors.toList());
     }
 }
